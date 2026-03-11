05a214b76a3f7105f754a4023cf0e5bc79fbcbc1
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-11 23:37:06 +0800

    Update Date.h

--- Date.h
+++ Date.h
@@ -9,25 +9,25 @@ public:
     Date() = default;
     Date(int day, int month, int year);
 
-    int getDay() const noexcept { return day_; }
+    int getDay()   const noexcept { return day_; }
     int getMonth() const noexcept { return month_; }
-    int getYear() const noexcept { return year_; }
+    int getYear()  const noexcept { return year_; }
 
     void setDay(int d);
     void setMonth(int m);
     void setYear(int y);
 
     bool operator==(const Date& other) const noexcept;
-    bool operator<(const Date& other) const noexcept;
-    bool operator>(const Date& other) const noexcept { return other < *this; }
+    bool operator!=(const Date& other) const noexcept { return !(*this == other); }
+    bool operator< (const Date& other) const noexcept;
 
 private:
     bool isValid() const noexcept;
-    bool isLeapYear(int y) const noexcept;
+    bool isLeapYear() const noexcept;
 
-    int day_ = 1;
+    int day_   = 1;
     int month_ = 1;
-    int year_ = 1900;
+    int year_  = 1900;
 };
 
 std::istream& operator>>(std::istream& is, Date& date);
