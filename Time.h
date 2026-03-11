8ddb694d7a99ea00fd12494369f386d9b2edc50e
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-03-11 22:53:48 +0800

    Update Time.h

--- Time.h
+++ Time.h
@@ -1,190 +1,63 @@
 #ifndef TIME_H
 #define TIME_H
+
 #include <iostream>
-#include <sstream>
-#include <fstream>
 
-
-    /**
-     * @class Time
-     * @brief This class is utilized to store and contain Time information such as hour, minute, second.
-     *
-     *
-     * @author  William Halling
-     * @version 0.2
-     * @date    April 2024
-     */
+/**
+ * @class Time
+ * @brief This class is used to store and manipulate time information (hour, minute, second).
+ *
+ * @author William Halling
+ * @version 0.3
+ * @date April 2024
+ */
 class Time
 {
-    public:
+public:
+    /// Default constructor: initializes time to 00:00:00
+    Time();
 
-            /**
-             * @brief Time Default constructor.
-             * Used to create a time object
-             */
-        Time();
+    /// Parameterized constructor
+    Time(int hour, int minute, int second);
 
+    /// Copy constructor
+    Time(const Time& otherTime);
 
-            /**
-             * @brief Time Parameterized constructor.
-             *
-             *
-             * @param newHour   int value to set the hour
-             * @param newMinute int value to set the minute
-             * @param newSecond int value to set the second
-             */
-        Time(int newHour, int newMinute, int newSecond);
+    /// Destructor
+    ~Time();
 
+    /// Assignment operator
+    const Time& operator=(const Time& otherTime);
 
-            /**
-             * @brief Time Copy Constructor
-             *
-             *
-             * @param newTime The time object to copy data from
-             * @return a copy of a time object
-             */
-        Time(const Time& newTime);
+    /// Equality operator
+    bool operator==(const Time& otherTime) const;
 
+    /// Inequality operator
+    bool operator!=(const Time& otherTime) const;
 
-            /**
-             * @brief Time Destructor
-             * Used to destroy a time object and release resources.
-             *
-             *
-             * @return destroyed time object / NA
-             */
-        ~Time();
+    /// Getters
+    int getHour() const;
+    int getMinute() const;
+    int getSecond() const;
 
+    /// Setters
+    void setHour(int hour);
+    void setMinute(int minute);
+    void setSecond(int second);
 
-            /**
-             * @brief Asignment operator.
-             * Used to assign the values of a Time object into another Time object, allows for deep copying of data
-             *
-             *
-             * @return copy of Date
-             */
-        const Time& operator=(const Time& otherTime);
+    /// Validation functions
+    bool validateHour(int hour) const;
+    bool validateMinute(int minute) const;
+    bool validateSecond(int second) const;
 
-
-            /**
-             * @brief  comparison operator.
-             * Used to compare one Time object against another Time object
-             *
-             *
-             * @param  otherTime - Time object to compare against
-             * @return bool true if one Time object equals another Time object
-             */
-        bool operator==(const Time& otherTime) const;
-
-
-
-            /**
-             * @brief  Not Equals operator
-             * Used to check that one time object doesnt equal another time object
-             *
-             *
-             * @param  otherTime - Time object to compare against
-             * @return bool true if Time are not equal else false
-             */
-        bool operator!=(const Time& otherTime) const;
-
-
-        bool operator>(const Time& otherTime) const;
-
-
-
-        bool operator<(const Time& otherTime) const;
-
-
-            /**
-             * @brief getHour
-             * Used to get the hour of a time object
-             *
-             *
-             * @return const int m_Hour
-             */
-        int getHour() const;
-
-
-            /**
-             * @brief getMinute
-             * Used to get the Minute of a time object
-             *
-             *
-             * @return const int m_Minute
-             */
-        int getMinute() const;
-
-
-            /**
-             * @brief getSecond
-             * Used to get the Second of a time object
-             *
-             *
-             * @return const int m_Second
-             */
-        int getSecond() const;
-
-
-            /**
-             * @brief setHour
-             * Used to set the Hour of a time object
-             *
-             *
-             * @param int newHour
-             */
-        void setHour(int newHour);
-
-
-            /**
-             * @brief setMinute
-             * Used to set the Minute of a time object
-             *
-             *
-             * @param int newMinute
-             */
-        void setMinute(int newMinute);
-
-
-            /**
-             * @brief setSecond
-             * @brief Used to set the Second of a time object
-             *
-             *
-             * @param int newSecond
-             */
-        void setSecond(int newSecond);
-
-
-    private:
-
-            /**
-             * @brief setSecond
-             * Used to set the Second of a time object
-             *
-             *
-             * @param int newSecond
-             * @return true if the time is between 0 and 59 false otherwise
-             */
-        bool isValidTime(int newTime) const;
-
-
-            /**
-             * @brief isValidHour()
-             * Used to check if the hour is between 0 - 23
-             *
-             *
-             * @param checkHour - the hour to check
-             * @return true if the hour is between 0 and 23 false otherwise
-             */
-        bool isValidHour(int checkHour) const;
-
-
-        int m_Hour;   /// int value for the hour as a value between 1 and 23
-        int m_Minute; /// int value for the hour as a value between 0 and 60
-        int m_Second; /// int value for the hour as a value between 0 and 60
+private:
+    int m_Hour;   /// Hour of the day (0-23)
+    int m_Minute; /// Minute of the hour (0-59)
+    int m_Second; /// Second of the minute (0-59)
 };
 
-std::istream & operator >>(std::istream& inputs, Time& t);
+// Stream operators
+std::istream& operator>>(std::istream& in, Time& t);
+std::ostream& operator<<(std::ostream& out, const Time& t);
 
 #endif // TIME_H
