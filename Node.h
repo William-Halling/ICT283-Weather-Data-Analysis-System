bf051b70d68546576caf8552d738211425aca53e
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:14:36 +0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,17 +1,25 @@
-#pragma once
+#ifndef NODE_H
+#define NODE_H
+
 #include <memory>
 #include <utility>
 
+/**
+ * @struct Node
+ * @brief Template node for a Binary Search Tree using RAII for memory management.
+ */
 template<typename T>
-struct Node 
-{
+struct Node {
     T data;
-    std::unique_ptr<Node> left;
-    std::unique_ptr<Node> right;
+    std::unique_ptr<Node<T>> left;
+    std::unique_ptr<Node<T>> right;
 
-    // explicit prevents accidental implicit type conversions
-    explicit Node(T storedData) : data(std::move(storedData)) {}
-    
-    // Default destructor is safe; unique_ptr automatically cleans up children
-    ~Node() = default; 
+    // Explicit constructor prevents accidental type conversions
+    explicit Node(T value) 
+        : data(std::move(value)), left(nullptr), right(nullptr) {}
+
+    // No manual destructor needed; unique_ptr handles cleanup
+    ~Node() = default;
 };
+
+#endif
