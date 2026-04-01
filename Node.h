2c7bee29edd041386feb4200c078a7533dc21805
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:13:03 +0800

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,47 +1,17 @@
-#ifndef NODE_H
-#define NODE_H
+#pragma once
+#include <memory>
+#include <utility>
 
-
-    /**
-     * @class  Node
-     * @brief  A struct for the node in our binary search tree.
-     * What encapsulation is used and why:
-     *
-     *
-     * @date   May 2024
-     * @author William Halling
-     */
-template<class T>
-struct Node
+template<typename T>
+struct Node 
 {
-    Node(T storedData);
+    T data;
+    std::unique_ptr<Node> left;
+    std::unique_ptr<Node> right;
 
-   ~Node();
-
-    T thisData;  /// The data going into the BST
-    Node* left;  /// Left Hand side Node
-    Node* right; /// Right Hand side Node
+    // explicit prevents accidental implicit type conversions
+    explicit Node(T storedData) : data(std::move(storedData)) {}
+    
+    // Default destructor is safe; unique_ptr automatically cleans up children
+    ~Node() = default; 
 };
-
-
-/**
- * @brief Node initialized Constructor.
- * Used to create a node with data of any type
- */
-template<class T>
-Node<T>::Node(T storedData) : thisData(storedData), left(nullptr), right(nullptr){}
-
-
-/**
- * @brief Node Destructor
- * Used to delete a node releasing its resources when not in use
- */
-
-template<class T>
-Node<T>::~Node()
-{
-    left  = nullptr; ///Left Hand side Node
-
-    right = nullptr; ///Right Hand side Node
-}
-#endif // NODE_H
