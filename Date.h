250fa5c1996ef73ab6e8191e14f59e82ab19c050
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:16:32 +0800

    Update Date.h

--- Date.h
+++ Date.h
@@ -1,6 +1,5 @@
 #pragma once
-
-#include <iosfwd>
+#include <iostream>
 
 namespace weather {
 
@@ -9,21 +8,21 @@ public:
     Date() = default;
     Date(int day, int month, int year);
 
-    int getDay()   const noexcept { return day_; }
-    int getMonth() const noexcept { return month_; }
-    int getYear()  const noexcept { return year_; }
+    [[nodiscard]] int getDay()   const noexcept { return day_; }
+    [[nodiscard]] int getMonth() const noexcept { return month_; }
+    [[nodiscard]] int getYear()  const noexcept { return year_; }
 
     void setDay(int d);
     void setMonth(int m);
     void setYear(int y);
 
-    bool operator==(const Date& other) const noexcept;
-    bool operator!=(const Date& other) const noexcept { return !(*this == other); }
-    bool operator< (const Date& other) const noexcept;
+    [[nodiscard]] bool operator==(const Date& other) const noexcept;
+    [[nodiscard]] bool operator<(const Date& other) const noexcept;
+    [[nodiscard]] bool operator!=(const Date& other) const noexcept { return !(*this == other); }
 
 private:
-    bool isValid() const noexcept;
-    bool isLeapYear() const noexcept;
+    [[nodiscard]] bool isValid() const noexcept;
+    [[nodiscard]] bool isLeapYear() const noexcept;
 
     int day_   = 1;
     int month_ = 1;
