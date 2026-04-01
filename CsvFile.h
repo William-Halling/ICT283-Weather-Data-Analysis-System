6cfcecab6737ebced9aded7c71e63c4e9aa7bf95
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-04-01 21:15:45 +0800

    Update CsvFile.h

--- CsvFile.h
+++ CsvFile.h
@@ -4,13 +4,12 @@
 #include "../weather/WeatherRecord.h"
 #include "../container/BinarySearchTree.h"
 #include <string>
+#include <string_view>
 #include <map>
 
-namespace io 
-{
+namespace io {
 
-class CsvFile
-{
+class CsvFile {
 public:
     explicit CsvFile(TextFile source) : sourceFiles_(std::move(source)) {}
 
@@ -18,8 +17,9 @@ public:
 
 private:
     void parseFile(const std::string& filename, container::BinarySearchTree<weather::WeatherRecord>& tree);
-    void parseHeader(const std::string& header);
-    void parseRow(const std::string& row, weather::WeatherRecord& rec) const;
+    void parseHeader(std::string_view header);
+    // Passing by reference for efficiency
+    void parseRow(std::string_view row, weather::WeatherRecord& rec) const;
 
     std::map<std::string, int> columnIndices_;
     TextFile sourceFiles_;
