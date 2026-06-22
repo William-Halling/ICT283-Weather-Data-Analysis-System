b61c018453764f791f503c40ce1ba88829793993
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-23 05:48:20 +0800

    Update Options.h

--- Options.h
+++ Options.h
@@ -1,36 +1,29 @@
 #pragma once
-
-#include <iosfwd>
 #include <vector>
 #include <string>
 
-namespace ui
-{
+namespace ui {
 
-    enum class MenuOption 
-    {
-        WindStats = 1,
-        TempStats,
-        SolarCorrelation,
-        Quit = 5
-    };
+enum class MenuOption {
+    WindStats = 1,
+    TempStats,
+    SolarCorrelation,
+    Quit = 5
+};
 
-    class Options 
-    {
-    public:
-        void displayMenu() const;
-        MenuOption getUserChoice() const;
-    
-        int  getValidMonth() const;
-        int  getValidYear()  const;
-    
-        void showWindspeedStats(int month, int year, const std::vector<float>& windspeeds) const;
-        void showTemperatureStats(int month, int year, const std::vector<float>& temperatures) const;
-        void showSolarCorrelation(int month, const std::vector<float>& windspeeds, const std::vector<float>& temperatures, const std::vector<float>& solar) const;
-        void showNoData(int month, int year) const;
-    
-    private:
-        std::string monthName(int month) const;
-    };
+class Options {
+public:
+    void displayMenu() const;
+    MenuOption getUserChoice() const;
+    int getValidMonth() const;
+    int getValidYear() const;
 
-} // namespace ui
+    void showWindspeedStats(int month, int year, const std::vector<float>& windspeeds) const;
+    void showTemperatureStats(int month, int year, const std::vector<float>& temperatures) const;
+    void showSolarCorrelation(int month, const std::vector<float>& ws, const std::vector<float>& temp, const std::vector<float>& solar) const;
+
+private:
+    std::string monthName(int month) const;
+};
+
+}
