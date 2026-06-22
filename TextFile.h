860103854dfbd74ca4c26e8a5746043d9ed027ab
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-23 05:37:07 +0800

    Update TextFile.h

--- TextFile.h
+++ TextFile.h
@@ -1,25 +1,20 @@
 #pragma once
-
 #include <string>
 #include <vector>
 
-namespace io 
-{
+namespace io {
 
-    class TextFile 
-    {
-    public:
-        TextFile() = default;
-        explicit TextFile(std::vector<std::string> files) : files_(std::move(files)) {}
-    
-        const std::vector<std::string>& getFiles() const noexcept { return files_; }
-    
-        void loadFrom(const std::string& listFile);
-    
-    private:
-        static bool isCsvFilename(const std::string& name) noexcept;
-    
-        std::vector<std::string> files_;
-    };
+class TextFile {
+public:
+    TextFile() = default;
+    explicit TextFile(std::vector<std::string> files) : files_(std::move(files)) {}
 
-} // namespace io
+    [[nodiscard]] const std::vector<std::string>& getFiles() const noexcept { return files_; }
+    void loadFrom(const std::string& listFile);
+
+private:
+    static bool isCsvFilename(const std::string& name) noexcept;
+    std::vector<std::string> files_;
+};
+
+}
