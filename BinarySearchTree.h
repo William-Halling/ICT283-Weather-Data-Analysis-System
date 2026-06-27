0c94969d071b5efa9f0336bf80168c5032bf081c
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-27 22:12:14 +0800

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -14,11 +14,12 @@ public:
     BinarySearchTree() noexcept : root_(nullptr) {}
     ~BinarySearchTree() = default;
 
-    // Disable copying to maintain strict unique ownership rules
+    // Rule of 5: Disable copying
     BinarySearchTree(const BinarySearchTree&) = delete;
     BinarySearchTree& operator=(const BinarySearchTree&) = delete;
 
     BinarySearchTree(BinarySearchTree&& other) noexcept : root_(std::move(other.root_)) {}
+
     BinarySearchTree& operator=(BinarySearchTree&& other) noexcept 
     {
         if (this != &other) 
@@ -28,10 +29,12 @@ public:
         return *this;
     }
 
+
     void insert(const T& value) 
     {
         insertNode(root_, value);
     }
+
 
     void insert(T&& value)
     {
@@ -39,9 +42,18 @@ public:
     }
 
     template <typename F, typename... Args>
-    void inOrderTraverse(F&& func, Args&&... args) const 
-    {
+    void inOrderTraverse(F&& func, Args&&... args) const {
         inOrder(root_.get(), std::forward<F>(func), std::forward<Args>(args)...);
+    }
+
+    template <typename F, typename... Args>
+    void preOrderTraverse(F&& func, Args&&... args) const {
+        preOrder(root_.get(), std::forward<F>(func), std::forward<Args>(args)...);
+    }
+
+    template <typename F, typename... Args>
+    void postOrderTraverse(F&& func, Args&&... args) const {
+        postOrder(root_.get(), std::forward<F>(func), std::forward<Args>(args)...);
     }
 
 private:
@@ -66,7 +78,7 @@ private:
     }
 
     void insertNode(std::unique_ptr<Node<T>>& node, T&& value) 
-{
+    {
         if (!node) 
         {
             node = std::make_unique<Node<T>>(std::move(value));
@@ -84,23 +96,32 @@ private:
         }
     }
 
+    template <typename F, typename... Args>
+    void inOrder(Node<T>* node, F&& func, Args&&... args) const {
+        if (!node) return;
+        inOrder(node->left.get(), func, args...);
+        func(node->data, args...);
+        inOrder(node->right.get(), func, args...);
+    }
 
     template <typename F, typename... Args>
-    void inOrder(Node<T>* node, F&& func, Args&&... args) const 
-    {    
-        if (!node)
-        {    
-            return;
-        }
-        
-        inOrder(node->left.get(), std::forward<F>(func), std::forward<Args>(args)...);
-        func(node->data, std::forward<Args>(args)...);
-        inOrder(node->right.get(), std::forward<F>(func), std::forward<Args>(args)...);
+    void preOrder(Node<T>* node, F&& func, Args&&... args) const {
+        if (!node) return;
+        func(node->data, args...);
+        preOrder(node->left.get(), func, args...);
+        preOrder(node->right.get(), func, args...);
+    }
+
+    template <typename F, typename... Args>
+    void postOrder(Node<T>* node, F&& func, Args&&... args) const {
+        if (!node) return;
+        postOrder(node->left.get(), func, args...);
+        postOrder(node->right.get(), func, args...);
+        func(node->data, args...);
     }
 
     std::unique_ptr<Node<T>> root_;
 };
-
 } // namespace container
 
 #endif // BINARYSEARCHTREE_H
