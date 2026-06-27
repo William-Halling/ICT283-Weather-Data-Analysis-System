0acee6839a6ce2f9e82ad976cf9a1842076654bd
William Halling <60004450+William-Halling@users.noreply.github.com>
2026-06-27 22:23:43 +0800

    Update Collectors.h

--- Collectors.h
+++ Collectors.h
@@ -2,26 +2,31 @@
 #include "weather/WeatherRecord.h"
 #include <vector>
 
-namespace collectors {
-
-inline void collectWindSpeed(const weather::WeatherRecord& rec, int month, int year, std::vector<float>& dest) {
-    if (rec.date.getMonth() == month && rec.date.getYear() == year) {
-        dest.push_back(rec.windspeed);
+namespace collectors 
+{
+    inline void collectWindSpeed(const weather::WeatherRecord& rec, int month, int year, std::vector<float>& dest)
+    {
+        if (rec.date.getMonth() == month && rec.date.getYear() == year) 
+        {
+            dest.push_back(rec.windspeed);
+        }
     }
-}
-
-inline void collectTemperature(const weather::WeatherRecord& rec, int month, int year, std::vector<float>& dest) {
-    if (rec.date.getMonth() == month && rec.date.getYear() == year) {
-        dest.push_back(rec.temperature);
+    
+    inline void collectTemperature(const weather::WeatherRecord& rec, int month, int year, std::vector<float>& dest) 
+    {
+        if (rec.date.getMonth() == month && rec.date.getYear() == year)
+        {
+            dest.push_back(rec.temperature);
+        }
     }
-}
-
-inline void collectAllMetrics(const weather::WeatherRecord& rec, int month, std::vector<float>& ws, std::vector<float>& temp, std::vector<float>& solar) {
-    if (rec.date.getMonth() == month) {
-        ws.push_back(rec.windspeed);
-        temp.push_back(rec.temperature);
-        solar.push_back(rec.solarRadiation);
+    
+    inline void collectAllMetrics(const weather::WeatherRecord& rec, int month, std::vector<float>& ws, std::vector<float>& temp, std::vector<float>& solar)
+    {
+        if (rec.date.getMonth() == month)
+        {
+            ws.push_back(rec.windspeed);
+            temp.push_back(rec.temperature);
+            solar.push_back(rec.solarRadiation);
+        }
     }
-}
-
 }
